8c7d878048f76261b44b5fa2d56bc6b17a52c2e2
harin <harinsd168@gmail.com>
2025-06-01 23:35:31 +0900

    모달 창 구현, 글쓰기 페이지 css 수정 필요

--- W-Writing/main.js
+++ W-Writing/main.js
@@ -1,5 +1,6 @@
 const startBtn = document.querySelector('.startBtn');
 
 startBtn.addEventListener('click', () => {
-    document.querySelector('.bg').classList.remove('set-modal');
+    document.querySelector('.bg').classList.add('close');
+    document.getElementById('main').style.display = 'block';
 });
