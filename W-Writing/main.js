e87fae8378153450d7ee11fc43e81056fa1c7cb3
harinsd404 <lohyen09@gmail.com>
2025-06-04 15:01:46 +0900

    시간 저장 형식 수정

--- W-Writing/main.js
+++ W-Writing/main.js
@@ -13,11 +13,22 @@ document.querySelector('.endBtn').addEventListener('click', (e) => {
     const who = document.querySelector('.who').value.trim();
     const letter = document.querySelector('.letter').value.trim();
 
+    let today = new Date(); 
+    
+    let year = today.getFullYear();
+    let month = (today.getMonth() + 1).toString().padStart(2, '0');
+    let day = today.getDay().toString().padStart(2, '0');
+    let hours = today.getHours().toString().padStart(2, '0');
+    let minutes = today.getMinutes().toString().padStart(2, '0'); 
+    
+    const date = `${year}-${month}-${day} ${hours}:${minutes}`;
+    console.log(date);
+
     if (who && letter) {
         localStorage.setItem('letterData', JSON.stringify({
             who,
             letter,
-            date: new Date().toLocaleString()
+            date
         }));
         window.location = './letter-page.html';
         }
